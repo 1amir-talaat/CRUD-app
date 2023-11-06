6b85956f2fdce9728cdcd4d57b2ce3c5e469fa49
amir talaat <hsiwh2be872b@gmail.com>
2023-11-06 22:41:09 +0200

    init commet 🚀

---
+++ client/app.js
@@ -0,0 +1,64 @@
+const updatePodect = (products) => {
+  const table = document.getElementById("table")
+  let productsList = ''
+  products.forEach(product => {
+    productsList += `<tr>
+        <td>${product.name}</td>
+        <td>${product.price}</td>
+        <td>${product.description}</td>
+        <td>
+        <button class="btn btn-danger" onclick=deletePodect(${product.id})>Delete</button>
+        </td></tr>`
+  });
+
+  table.innerHTML = productsList
+}
+
+const getProdects = async () => {
+  await fetch('http://localhost:3000/products')
+    .then(
+      (res) => res.json()
+    )
+    .then(
+      (data) => updatePodect(data)
+    )
+}
+
+const addProduct = async (e) => {
+  e.preventDefault()
+
+  const data = {
+    name: e.target.prodectName.value,
+    price: e.target.prodectPrice.value,
+    description: e.target.prodectDesc.value,
+  }
+
+  await fetch('http://localhost:3000/addproduct', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+
+  e.target.prodectName.value = ''
+  e.target.prodectPrice.value = ''
+  e.target.prodectDesc.value = ''
+
+  getProdects()
+
+}
+
+const deletePodect = async (id) => {
+  await fetch(`http://localhost:3000/deletprodect/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+
+  getProdects()
+
+}
+
+getProdects()
---
+++ server/server.js
@@ -0,0 +1,62 @@
+import express, { json } from 'express';
+import { createConnection } from 'mysql2';
+import cors from 'cors';
+
+const app = express()
+app.use(json())
+app.use(cors())
+
+const db = createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'test'
+})
+
+app.get('/products', (req, res) => {
+  let query = "SELECT * FROM `Products`"
+  db.query(query, (err, data) => {
+    if (err) {
+      res.status(400).send("cant get data")
+    }
+    res.status(200).json(data)
+  })
+})
+
+app.post("/addproduct", (req, res) => {
+
+  let { name, price, description } = req.body
+  let query = "INSERT INTO `Products`(`name`, `price`, `description`) VALUES (?,?,?)"
+
+  db.query(query, [name, price, description], (err, data) => {
+    if (err) {
+      res.status(400).send("error")
+
+      console.log(err);
+    }
+    else {
+      res.status(200).send("added")
+    }
+  })
+})
+
+app.delete('/deletprodect/:id', (req, res) => {
+  const id = req.params.id
+
+  let query = "DELETE FROM `Products` WHERE `id` = " + id
+
+  db.query(query, (err, data) => {
+    if (err) {
+      res.status(400).send("error")
+
+      console.log(err);
+    }
+    else {
+      res.status(200).send("deleted")
+    }
+  })
+})
+
+app.listen(3000, () => {
+  console.log("server start at port 3000");
+})
