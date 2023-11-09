9292f0f83d3dc2bec6d4cc30085429a3ba27eb6b
amir talaat <hsiwh2be872b@gmail.com>
2023-11-09 19:00:02 +0200

    .

--- client/app.js
+++ client/app.js
@@ -1,5 +1,5 @@
 let prodects = [];
-let currntProdect = null
+let currntProdect = null;
 
 const updatePodectsList = (data) => {
   const table = document.getElementById("table");
@@ -12,20 +12,19 @@ const updatePodectsList = (data) => {
         <td>${product.description}</td>
         <td>
         <button class="btn btn-danger me-3" onclick=deleteProduct(${product.id})>Delete</button>
-        <button class="btn btn-success" onclick=editProduct(${product.id})>update</button>
+        <button class="btn btn-success" onclick=editProduct(${product.id})>edit</button>
         </td></tr>`;
   });
 
   table.innerHTML = productsList;
 };
 
-
 const getProducts = async () => {
-  await fetch('http://localhost:3000/products')
-    .then(res => res.json())
-    .then(data => updatePodectsList(data))
-    .catch(error => console.error("Error fetching products: " + error));
-}
+  await fetch("http://localhost:3000/products")
+    .then((res) => res.json())
+    .then((data) => updatePodectsList(data))
+    .catch((error) => console.error("Error fetching products: " + error));
+};
 
 const resetForm = () => {
   document.querySelector("#prodectName").value = "";
@@ -33,45 +32,43 @@ const resetForm = () => {
   document.querySelector("#prodectDesc").value = "";
   document.querySelector("#add").style.display = "block";
   document.querySelector("#update").style.display = "none";
-}
+};
 
 const getFormValues = () => {
   let values = {
     name: document.querySelector("#prodectName").value,
     price: document.querySelector("#prodectPrice").value,
     description: document.querySelector("#prodectDesc").value,
-  }
+  };
 
-  return values
-}
+  return values;
+};
 
 const addProduct = async (e) => {
   e.preventDefault();
 
-  const data = getFormValues()
+  const data = getFormValues();
 
-  await fetch('http://localhost:3000/addproduct', {
-    method: 'POST',
+  await fetch("http://localhost:3000/addproduct", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(data)
-  })
-    .then(() => {
-      resetForm();
-      getProducts();
-    })
-}
+    body: JSON.stringify(data),
+  }).then(() => {
+    resetForm();
+    getProducts();
+  });
+};
 
 const deleteProduct = async (id) => {
   await fetch(`http://localhost:3000/deletprodect/${id}`, {
-    method: 'DELETE',
+    method: "DELETE",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
-  })
-    .then(() => getProducts())
-}
+  }).then(() => getProducts());
+};
 
 const editProduct = (id) => {
   const updateButton = document.querySelector("#update");
@@ -79,32 +76,34 @@ const editProduct = (id) => {
   updateButton.style.display = "block";
   addButton.style.display = "none";
 
-  currntProdect = id
+  let product = prodects.filter((prodect) => {
+    return prodect.id == id;
+  })[0];
 
-}
+  document.querySelector("#prodectName").value = product.name;
+  document.querySelector("#prodectPrice").value = product.price;
+  document.querySelector("#prodectDesc").value = product.description;
+
+  currntProdect = id;
+};
 
 const handelFormEditProdect = () => {
-
-  const updatedData = getFormValues()
+  const updatedData = getFormValues();
 
   fetch(`http://localhost:3000/updateproduct/${currntProdect}`, {
-    method: 'PUT',
+    method: "PUT",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(updatedData),
-  })
-    .then(() => {
-      resetForm();
-      getProducts();
-    })
-
-}
-
-console.log(currntProdect);
+  }).then(() => {
+    resetForm();
+    getProducts();
+  });
+};
 
 document.querySelector("#add").addEventListener("click", addProduct);
-document.querySelector("#update").addEventListener("click", handelFormEditProdect)
+document.querySelector("#update").addEventListener("click", handelFormEditProdect);
 
 resetForm();
 getProducts();
